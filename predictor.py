d68cec8dfef05f11b7401c84be9eeb1518d7ba72
trkohler <troyalekson@gmail.com>
2020-11-03 15:34:27 +0200

    predictor for danny

--- predictor.py
+++ predictor.py
@@ -12,22 +12,16 @@ regex = re.compile("[%s]" % re.escape(string.punctuation))
 
 
 class LdaPredictor:
-    def __init__(self, lda_path, dict_path, bigram_path, trigram_path):
+    def __init__(self, lda_path, dict_path):
         """
         lda_path - path to lda model
         dict_path - path to dict
-        bigram_path - path to bigrams
-        trigram_path - path to trigrams
 
         param: lda_path str
         param: dict_path str
-        param: bigram_path str
-        param: trigram_path str
         """
         self.dictionary = corpora.Dictionary.load(dict_path)
         self.lda = LdaMulticore.load(lda_path)
-        self.bigram_path = bigram_path
-        self.trigram_path = trigram_path
 
     def clean(self, text):
         text = regex.sub("", text)
@@ -37,36 +31,21 @@ class LdaPredictor:
         return " ".join(text)
 
     def show_topics(self):
-        pprint(self.lda.print_topics(num_words=20))
+        pprint(self.lda.print_topics(num_words=30))
 
-    def bigram(self, text):
-        bigram = Phrases.load(self.bigram_path)
-        trigram = Phrases.load(self.trigram_path)
-        text_clean = text
-        for idx in range(len(text_clean)):
-            for token in bigram[text_clean[idx]]:
-                if "_" in token:
-                    text_clean[idx].append(token)
-            for token in trigram[text_clean[idx]]:
-                if "_" in token:
-                    text_clean[idx].append(token)
-        return text_clean
 
     def predict(self, text):
         clean_text = self.clean(text).split()
-        bigram = self.bigram([clean_text])
-        new_review_bow = self.dictionary.doc2bow(bigram[0])
+        new_review_bow = self.dictionary.doc2bow(clean_text)
         new_review_lda = self.lda[new_review_bow]
         return sorted(new_review_lda, reverse=True, key=operator.itemgetter(1))
 
 
-lda_path = "./model/mallet_model.lda"
-dict_path = "./model/dictionary.dict"
-bigram_path = "./model/bigram.phs"
-trigram_path = "./model/trigram.phs"
-lda = LdaPredictor(lda_path, dict_path, bigram_path, trigram_path)
-text = "A church building or church house, often simply called a church, is a building used for Christian religious activities, particularly for Christian worship services. The term is often used by Christians to refer to the physical buildings where they worship, but it is sometimes used as an analogy to refer to buildings of other religions.[1] In traditional Christian architecture, a church interior is often structured in the shape of a Christian cross. When viewed from plan view the vertical beam of the cross is represented by the center aisle and seating while the horizontal beam and junction of the cross is formed by the bema and altar."
+lda_path = "model/mallet_to_native.lda"
+dict_path = "model/dictionary.dict"
+lda = LdaPredictor(lda_path, dict_path)
+text = "I didn't have particular aspirations"
 predict = lda.predict(text)
 print(predict)
 
-lda.show_topics()
+# lda.show_topics()
